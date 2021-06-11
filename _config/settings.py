6e53e5ee5ad54835f3be7ed2386c64cd7f040a6e
sebbby <seb.beitel@gmail.com>
2021-06-10 23:18:48 -0400

    fix issues caused by changing the repo name

--- _config/settings.py
+++ _config/settings.py
@@ -83,7 +83,7 @@ AUTH_USER_MODEL = "users.User"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
